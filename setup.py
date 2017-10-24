7764ab9a2bfaa4a10f274319e700ca026424d213
Joshua Harlow <jxharlow@godaddy.com>
2017-10-24 14:07:20 -0700

    Remove extra setup.py spaces

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name = "tinyjenkins",
-    version = "0.0.1",
-    author = "Josh Harlow",
-    author_email = "harlowja@gmail.com",
-    description = "A tiny jenkins client library",
-    license = "ASL",
-    keywords = "jenkins",
+    name="tinyjenkins",
+    version="0.0.1",
+    author="Josh Harlow",
+    author_email="harlowja@gmail.com",
+    description="A tiny jenkins client library",
+    license="ASL",
+    keywords="jenkins",
     packages=['tinyjenkins'],
     long_description=open("README").read(),
     install_requires=['requests', 'munch', 'oslo_utils'],
